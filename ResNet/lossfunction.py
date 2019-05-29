6a0ee0a6baa532863ffb4a9792d1c1e20f7de56b
Yao Yuan <distantyy@gmail.com>
2019-05-29 20:48:30 +0800

    Update lossfunction.py

--- ResNet/lossfunction.py
+++ ResNet/lossfunction.py
@@ -9,31 +9,26 @@ from torch.autograd import Variable
 
 class FocalLoss(nn.Module):
 
-	def __init__(self, num_classes, alpha=None, gamma=2):
+	def __init__(self, alpha=0.25, gamma=2):
 		super(FocalLoss, self).__init__()
-		self.num_classes = num_classes
-		if alpha is None:
-			self.alpha = torch.ones(self.num_classes, 1)
-		else:
-			self.alpha = torch.tensor(alpha)
+		self.alpha = torch.tensor(alpha)
 		self.gamma = gamma
-		self.sm = nn.Softmax(dim=1)
 
 	def forward(self, inputs, labels):
-		prob = self.sm(inputs)
+		prob = Functional.softmax(inputs, dim=1)
 		one_hot = torch.zeros(inputs.size())
 		if inputs.is_cuda:
 			one_hot = one_hot.cuda()
 		one_hot.scatter_(1, labels.view(-1, 1).data, 1)
 		prob = (prob * one_hot).sum(1).view(-1, 1)
-		ce = prob.log()
+		ce = (prob).log()
 
 		loss = -self.alpha * (torch.pow((1 - prob), self.gamma)) * ce
 		return loss.mean()
 
 class Arcface(nn.Module):
 
-	def __init__(self, in_features, num_classes, s=30.0, m=0.50):
+	def __init__(self, in_features, num_classes, s=64.0, m=0.50):
 		super(Arcface, self).__init__()
 		self.in_features = in_features
 		self.out_features = num_classes
@@ -49,12 +44,10 @@ class Arcface(nn.Module):
 		sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
 		phi = cosine * self.cosm - sine * self.sinm
 		phi = torch.where(cosine+self.cosm>0, phi, cosine-self.sinm*self.m)
-
 		one_hot = torch.zeros(cosine.size())
 		if inputs.is_cuda:
 			one_hot = one_hot.cuda()
 		one_hot.scatter_(1, labels.view(-1, 1).data, 1)
-
 		output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
 		output *= self.s
 
